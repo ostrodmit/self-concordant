fa2961580bb6ab3acd9fdf3f3a096f5eca10e0fd
Dmitrii Ostrovskii <ostrodmit@gmail.com>
2019-10-17 22:57:49 -0700

    Averaging added, norm of theta_true added

--- generate_data_class.m
+++ generate_data_class.m
@@ -1,4 +1,4 @@
-function [X,Y] = generate_data_class(d,n,theta,key) % generate data for classification
+function [X,Y] = generate_data_class(d,n,theta,key,pNoise) % generate data for classification
 X = randn(n,d);
 Y = zeros(n,1);
 for i = 1:n,
@@ -11,6 +11,9 @@ for i = 1:n,
         p = 1/(1 + exp(-eta));
         r = rand;
         Y(i) = sign(p-r);
+    end
+    if nargin > 4, % additional noise with probability pNoise
+        Y(i) = Y(i) * sign(rand-pNoise);
     end
 end
 end
--- run_sims.m
+++ run_sims.m
@@ -1,13 +1,17 @@
 d = 10;
-theta_true = randn(d,1);
-nn = uint16(logspace(1,3.5,40));
-N = 10000;
+R = 10.0;
+nn = uint16(logspace(1.5,3.5,30));
+N = 10000; % population size
+T = 10; % number of trials
 
 %% Logistic data, classification
 key = 'logistic';
+pNoise = 0.2;
+% key = '0-1';
 
 %% Generate population distribution
-[XX,YY] = generate_data_class(d,N,theta_true,key); 
+theta_true = R * ones(d,1);
+[XX,YY] = generate_data_class(d,N,theta_true,key,pNoise); 
 prisk_log = @(theta)emp_risk(theta,XX,YY,@logistic);
 prisk_sc = @(theta)emp_risk(theta,XX,YY,@sc_class);
 
@@ -15,12 +19,19 @@ problem.x0 = 0.001 * randn(d,1);
 
 %% Minimize population risk
 % logistic 
-problem.x0 = theta_true + 0.0001*ones(d,1); % ??? warm-start from the true parameter value
+problem.options = optimoptions('fmincon','Algorithm','interior-point',...
+        'SpecifyObjectiveGradient',true,'CheckGradients',false,...
+        'Display','notify-detailed');%'PlotFcn','optimplotfval');
+problem.solver = 'fmincon';
+% Box constraints for ill-conditioned case (n <= d)
+problem.A = [eye(d);-eye(d)]; 
+problem.b = 1e2*ones(2*d,1);
+problem.x0 = theta_true + 0.0001*ones(d,1); % warm-start from the true parameter value
 problem.objective = prisk_log;
-[ptheta_log,prisk_opt_log,EXITFLAG,OUTPUT,pgrad_log] = fminunc(problem);
+[ptheta_log,prisk_opt_log,EXITFLAG,OUTPUT,pgrad_log] = fmincon(problem);
 % SC loss
 problem.objective = prisk_sc;
-[ptheta_sc,prisk_opt_sc,EXITFLAG,OUTPUT,pgrad_sc] = fminunc(problem);
+[ptheta_sc,prisk_opt_sc,EXITFLAG,OUTPUT,pgrad_sc] = fmincon(problem);
 
 %% ERM fitting
 excess_log = zeros(size(nn));
@@ -29,33 +40,34 @@ excess_apx = zeros(size(nn));
 nIdx = 0;
 for n = nn,
     nIdx = nIdx + 1;
-    %% Generate training sample
-    [X,Y] = generate_data_class(d,n,theta_true,key);
-    %gscatter(X(:,1),X(:,2),Y(:));
-    erisk_log = @(theta)emp_risk(theta,X,Y,@logistic);
-    erisk_sc = @(theta)emp_risk(theta,X,Y,@sc_class);
+    for t = 1:T,
+        %% Generate training sample
+        %
+        [X,Y] = generate_data_class(d,n,theta_true,key,pNoise);
+        %gscatter(X(:,1),X(:,2),Y(:));
+        erisk_log = @(theta)emp_risk(theta,X,Y,@logistic);
+        erisk_sc = @(theta)emp_risk(theta,X,Y,@sc_class);
 
-    %% Minimize empirical risk for logistic and SC losses
-    problem.options = optimoptions('fminunc','Algorithm','trust-region',...
-        'SpecifyObjectiveGradient',true);
-    problem.solver = 'fminunc';
-    %
-    problem.objective = erisk_log;
-    [etheta_log,erisk_opt_log,EXITFLAG,OUTPUT,egrad_log] = fminunc(problem);
-    %
-    problem.objective = erisk_sc;
-    [etheta_sc,erisk_opt_sc,EXITFLAG,OUTPUT,egrad_sc] = fminunc(problem);
-    
-    %% Compute excess risks
-    % excess_log = prisk_log(etheta_log) - prisk_log(theta_true) % logistic loss
-    excess_log(nIdx) = prisk_log(etheta_log) - prisk_log(ptheta_log); % logistic loss
-    excess_sc(nIdx) = prisk_sc(etheta_sc) - prisk_sc(ptheta_sc); % SC loss
-    excess_apx(nIdx) = prisk_log(etheta_sc) - prisk_log(ptheta_log); % ``approximated'' risk
+        %% Minimize empirical risk for logistic and SC losses
+        %
+        problem.objective = erisk_log;
+        [etheta_log,erisk_opt_log,EXITFLAG,OUTPUT,egrad_log] = fmincon(problem);
+        %
+        problem.objective = erisk_sc;
+        [etheta_sc,erisk_opt_sc,EXITFLAG,OUTPUT,egrad_sc] = fmincon(problem);
+
+        %% Compute excess risks
+        excess_log(t,nIdx) = prisk_log(etheta_log) - prisk_log(ptheta_log); % logistic loss
+        excess_sc(t,nIdx) = prisk_sc(etheta_sc) - prisk_sc(ptheta_sc); % SC loss
+        %     excess_apx(nIdx) = prisk_log(etheta_sc) - prisk_log(ptheta_log); % ``approximated'' risk
+    end
 end
+ave_excess_log = sum(excess_log,1)/T;
+ave_excess_sc = sum(excess_sc,1)/T;
 
-%% Plot excess risks
+%% Plot excess risks averaged over T experiments
 nn_double = double(nn);
-plot(log10(nn_double),log(excess_log),log10(nn_double),log(excess_sc),log10(nn_double),log(excess_apx));
-legend('logistic','SC','transfer');
+plot(log10(nn_double),log10(ave_excess_log),log10(nn_double),log10(ave_excess_sc));%,log10(nn_double),log(excess_apx));
+legend('logistic','SC');%,'transfer');
 xlabel('log(n)');
 ylabel('log(excess_risk)');
