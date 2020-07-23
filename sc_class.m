4e60d967a6aa68452fe589350193089dba2bac35
Dmitrii Ostrovskii <ostrodmit@gmail.com>
2020-07-22 18:17:39 -0700

    Verified the derivative of SC loss

--- sc_class.m
+++ sc_class.m
@@ -1,10 +1,10 @@
 %% SC classification loss and its derivatives
 function [l,g,h] = sc_class(y,eta)
 z = y*eta;
-r = sqrt(1 + z^2);
+r = sqrt(1+z^2);
 if z ~= 0,
     l = 2 + 1/log(4) * (-1-z + r + log((r-1)/(2*z^2)));
-    g = y*(r-z-1)/z/log(4);
+    g = y*(r-z-1)/z/log(4); % verified through partial differences
     h = (1-1/r)/z^2/log(4);
 else
     l = 1;
